6c58cb85716da1788727385e182da7b17ea514c0
Ezequiel Angeloni <e.zeangeloni@gmail.com>
2023-02-09 17:06:06 +0100

    add bookmark

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
-const bookmarksImages = ["img/bookmarkON.svg", "img/bookmark_OFF.svg"];
+// Bookmark Toggle ON/OFF
+const iconBookmark = document.querySelector('[data-js="icon-bookmark"]');
 
-const bookmark = document.getElementById("bookmark1");
+iconBookmark.addEventListener("click", () => {
+  iconBookmark.classList.toggle("change-bookmark");
+});
 
-let toggle = false;
+// Show/Hide Answer
 
-bookmark.addEventListener("click", () => {
-  toggle = !toggle;
-  if (toggle) {
-    bookmark.src = bookmarksImages[1];
+const answerButton = document.querySelector('[data-js="button-answer"]');
+const answer = document.querySelector('[data-js="answer-hidden"]');
+
+answerButton.addEventListener("click", () => {
+  answer.classList.toggle("answer--hiden");
+
+  if (answer.classList.value === "hidden") {
+    answerButton.textContent = "Show Answer";
   } else {
-    bookmark.src = bookmarksImages[0];
+    answerButton.textContent = "Hide Answer";
   }
 });
